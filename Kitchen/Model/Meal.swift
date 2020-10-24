8154a816e3ef5e8ba5409538a9e9ecb44c32775d
Daniel Parra <danialepaco@hotmail.com>
2020-10-24 08:56:02 -0500

    added Meal model

---
+++ Kitchen/Model/Meal.swift
@@ -0,0 +1,31 @@
+//
+//  Meal.swift
+//  Kitchen
+//
+//  Created by Daniel Parra on 10/24/20.
+//
+
+import Foundation
+
+struct Meals: Decodable {
+    let meals: [Meal]
+}
+
+struct Meal: Decodable {
+    let id: String
+    let thumb: String
+    let name: String
+    let category: String
+    let instructions: String
+    let video: String
+    var ingredients: [String] = []
+
+    enum CodingKeys: String, CodingKey {
+        case id = "idMeal"
+        case thumb = "strMealThumb"
+        case name = "strMeal"
+        case category = "strCategory"
+        case instructions = "strInstructions"
+        case video = "strYoutube"
+    }
+}
